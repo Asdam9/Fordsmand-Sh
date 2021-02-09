63ba5ddde9e97d20880b1d00cabf239d861b6148
Adam Fordsmand <indermandfrainderland@gmail.com>
2021-02-09 23:25:34 +0100

    added a lot

--- Titlebar.sh
+++ Titlebar.sh
@@ -2,8 +2,8 @@
 
 (
 while true; do
-	xsetroot -name "| $(cat /sys/class/power_supply/BAT0/capacity)% - $(cat /sys/class/power_supply/BAT0/status) | vol: $(pamixer --get-mute > /dev/null && echo "muted ($(pamixer --get-volume)%)" || echo "$(pamixer --get-volume)%") | $(curl -s "wttr.in/?format=2&M" | tr -cd "a-zA-Z0-9°+.,/\- " | sed -e 's/^[ \t]*//') |  $(date +"%b %d - %H:%M")" 
-	sleep 1
+    xsetroot -name "| YT↓ : $( ls ~/Videos/downloadedYT/ | grep part | wc -l) | $(xbacklight | cut -f '1' -d '.')% | $(cat /sys/class/power_supply/BAT0/capacity)% - $(cat /sys/class/power_supply/BAT0/status) | vol: $(pamixer --get-mute > /dev/null && echo "muted ($(pamixer --get-volume)%)" || echo "$(pamixer --get-volume)%") | $(cat ~/Documents/Scripts/Fordsmand-Sh/weather.txt) | $(date +"%b %d - %H:%M:%S")" 
+    sleep 0.01
 done
 ) &
 
