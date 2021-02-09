eca0d7868ee77aa49109192b8368d16ea792548b
Adam Fordsmand <indermandfrainderland@gmail.com>
2021-02-10 00:03:02 +0100

    Updated titlebar and startup to fit desktop

--- Titlebar.sh
+++ Titlebar.sh
@@ -2,7 +2,7 @@
 
 (
 while true; do
-    xsetroot -name "| YT↓ : $( ls ~/Videos/downloadedYT/ | grep part | wc -l) | $(xbacklight | cut -f '1' -d '.')% | $(cat /sys/class/power_supply/BAT0/capacity)% - $(cat /sys/class/power_supply/BAT0/status) | vol: $(pamixer --get-mute > /dev/null && echo "muted ($(pamixer --get-volume)%)" || echo "$(pamixer --get-volume)%") | $(cat ~/Documents/Scripts/Fordsmand-Sh/weather.txt) | $(date +"%b %d - %H:%M:%S")" 
+    xsetroot -name " $([ -f ~/Videos/downloadedYT/ ] && echo "| YT↓ : $( ls ~/Videos/downloadedYT/ | grep part | wc -l)") $( [ "$(xbacklight)" != ""  ] && echo "| $(xbacklight | cut -f '1' -d '.')%") $( [ -f /sys/class/power_supply/BAT0/capacity ] && echo "| $(cat /sys/class/power_supply/BAT0/capacity)% - $(cat /sys/class/power_supply/BAT0/status) " )| vol: $(pamixer --get-mute > /dev/null && echo "muted ($(pamixer --get-volume)%)" || echo "$(pamixer --get-volume)%") | $(cat ~/Documents/Scripts/Fordsmand-Sh/weather.txt) | $(date +"%b %d - %H:%M:%S")" 
     sleep 0.01
 done
 ) &
